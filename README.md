1bc2739ae42bb3f3fdf38434d666e5b3d491139f
Marcelo Pera <marce@MarceloPera>
2024-03-05 19:40:17 -0300

    implementacoes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # my-code-life
 In this repository is all my code life
+
+### to-do
+Set PostgreSQL
+Set Redis
+Set MongoDB
+Set Kafka
---
+++ api/home/src/main/java/br/com/caminhada/OlhoVivoUtils.java
@@ -0,0 +1,18 @@
+package br.com.caminhada;
+
+import jakarta.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class OlhoVivoUtils {
+
+
+    //metodo que realiza uma req get para a api do olho vivo
+    public String getOlhoVivo() {
+        
+        
+
+
+
+        return "Olá, mundo! eu sou o novo projeto! Vamos caminhar juntos?";
+    }
+}
